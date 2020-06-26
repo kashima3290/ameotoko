8567c1f5e9c5eaac7dd170e810362fbdf8a177cd
kashimaraiki <raiki3290@gmail.com>
2020-06-26 12:22:24 +0900

    bot-header component

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,19 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="#fcb69f"
-      dark
-      src="https://picsum.photos/1920/1080?random"
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
-      <v-toolbar-title>LINE-Bot サーバー "雨男"</v-toolbar-title>
-    </v-app-bar>
+    <bot-header></bot-header>
 
     <v-main>
       <v-container
@@ -26,9 +13,13 @@
 </template>
 
 <script>
+  import BotHeader from './packs/home/bot-header';
   export default {
     props: {
       source: String,
+    },
+    components: {
+      BotHeader
     }
   }
 </script>
