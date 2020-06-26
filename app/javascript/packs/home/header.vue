997057d17c22780f89dfee6e365ea5156e5833e8
kashimaraiki <raiki3290@gmail.com>
2020-06-26 12:13:13 +0900

    Create header.vue

---
+++ app/javascript/packs/home/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar
+    app
+    color="#fcb69f"
+    dark
+    src="https://picsum.photos/1920/1080?random"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      ></v-img>
+    </template>
+    <v-toolbar-title>LINE-Bot サーバー "雨男"</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script >
+</script>
