c504b26f41ba1a67444b10790b028f0b34a33a0d
kashimaraiki <raiki3290@gmail.com>
2020-06-26 10:22:28 +0900

    basic layout

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,25 +1,28 @@
 <template>
-  <v-app id="app">
-    <v-btn>Vuetifyのボタン</v-btn>
-    <div>
-      <p>{{ message }}</p>
-    </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+  export default {
+    props: {
+      source: String,
     }
   }
-}
 </script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
