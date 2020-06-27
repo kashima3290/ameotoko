172f609bd659072fcf92044001f2425c25284311
kashimaraiki <raiki3290@gmail.com>
2020-06-27 22:06:55 +0900

    get API imformation

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -24,8 +24,16 @@
       }
     },
     created(){
+      this.todayWeather();
     },
-    methods: function(){
+    methods: {
+      todayWeather() {
+        let OPEN_WETHER_MAP_API = gon.open_weather_map_api;
+        let now_weather = axios.get('https://api.openweathermap.org/data/2.5/weather?q=Osaka-shi,jp&units=metric&lang=ja&APPID=' + OPEN_WETHER_MAP_API)
+        let forecast = axios.get('http://api.openweathermap.org/data/2.5/forecast?q=Osaka-shi,jp&units=metric&lang=ja&APPID=' + OPEN_WETHER_MAP_API)
+        console.log(now_weather)
+        console.log(forecast)
+      }
     },
     components: {
       BotHeader,
