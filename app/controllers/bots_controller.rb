f60c0d40468390777071344369fe5452791fe66a
kashimaraiki <raiki3290@gmail.com>
2020-06-29 13:41:04 +0900

    accesss rule change

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -1,13 +1,13 @@
 class BotsController < ApplicationController
   require 'line/bot'
-  require "open-uri"
   protect_from_forgery :except => [:callback]
 
   def index
 
     # 現在天気
-    now_weather_response = open("https://api.openweathermap.org/data/2.5/weather?q=Osaka-shi,jp&units=metric&lang=ja&APPID=#{ENV['OPEN_WETHER_MAP_API']}")
-    now_weather = JSON.parse(now_weather_response.read)["weather"][0]["description"]
+    now_weather_uri = URI.parse("https://api.openweathermap.org/data/2.5/weather?q=Osaka-shi,jp&units=metric&lang=ja&APPID=#{ENV['OPEN_WETHER_MAP_API']}")
+    now_weather_response = Net::HTTP.get_response(now_weather_uri)
+    now_weather = JSON.parse(now_weather_response.body)["weather"][0]["description"]
 
     # 天気予報
     # forecasts = []
