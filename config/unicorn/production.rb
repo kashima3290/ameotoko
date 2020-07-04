f6829677dcfa1678c22a5b3d0b15612ee165778e
kashimaraiki <raiki3290@gmail.com>
2020-07-04 15:38:11 +0900

    Update production.rb

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,7 +7,7 @@ $worker  = 2
 #リクエストを受け取るポート番号を指定。後述
   $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 #PIDの管理ファイルディレクトリ
-  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+  pid "#{app_shared_path}/tmp/pids/unicorn.pid"
 #エラーログを吐き出すファイルのディレクトリ
   $std_log = File.expand_path 'log/unicorn.log', $app_dir
 
@@ -18,7 +18,6 @@ $worker  = 2
   stdout_path $std_log
   timeout $timeout
   listen  $listen
-  pid $pid
 
 #ホットデプロイをするかしないかを設定
   preload_app true
