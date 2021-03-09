944a0c6e7f0518fad88797d541270dc77d2b9050
kashimaraiki <raiki3290@gmail.com>
2021-03-09 20:32:56 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Ameotoko
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.debug_exception_response_format = :default
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
