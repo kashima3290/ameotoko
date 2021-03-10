c71c1394d75f32aa0bacd78883d6d2b0cd02c918
kashimaraiki <raiki3290@gmail.com>
2021-03-11 08:00:37 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,14 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -29,5 +32,6 @@ module Ameotoko
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.api_only = true
   end
 end
