8780535162840f05d7257f4c0b17b77a17fe63c1
kashimaraiki <raiki3290@gmail.com>
2021-03-17 16:44:57 +0900

    Update main.js

--- ameotoko_front/src/main.js
+++ ameotoko_front/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// import BootstrapVue from 'bootstrap-vue'
+// app.use(BootstrapVue)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+app.mount('#app')
