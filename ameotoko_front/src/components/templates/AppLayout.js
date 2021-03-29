7003c38d34b59b07d2ed5c4a610f766f0f8bde7c
kashimaraiki <raiki3290@gmail.com>
2021-03-29 10:57:54 +0900

    Update AppLayout.js

--- ameotoko_front/src/components/templates/AppLayout.js
+++ ameotoko_front/src/components/templates/AppLayout.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -14,11 +20,15 @@ import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
+// pages
+import Login from "../pages/Login";
+import Weather from "../pages/Weather";
+import LineConfig from "../pages/LineConfig";
 
 const drawerWidth = 240;
 
@@ -156,10 +166,21 @@ export default function AppLayout() {
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-          </Grid>
-        </Container>
+          <Switch>
+            <Route path="/Weather">
+              <Weather />
+            </Route>
+            <Route path="/LineConfig">
+              <LineConfig />
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+          {/* <Container maxWidth="lg" className={classes.container}>
+            <Grid container spacing={3}>
+            </Grid>
+          </Container> */}
       </main>
     </div>
   );
