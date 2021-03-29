aa3a136e9c9ee8b7229d1af3f2e9cf019494b74c
kashimaraiki <raiki3290@gmail.com>
2021-03-29 10:57:51 +0900

    Update App.js

--- ameotoko_front/src/App.js
+++ ameotoko_front/src/App.js
@@ -1,50 +1,11 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Login from "./components/pages/Login";
-import Header from "./components/organisms/Header";
+import { BrowserRouter } from "react-router-dom";
 import AppLayout from "./components/templates/AppLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
       <AppLayout />
-      <Switch>
-        <Route path="/Login">
-          <Login />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-      <Link to="/">Back To Home</Link>
-      <br/>
-      <Link to="/Login">login</Link>
-      <br/>
-      <Link to="/about">about</Link>
     </BrowserRouter>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
