9186fc1bf7596ef8e890de29a8697c8d9878bfc6
kashimaraiki <raiki3290@gmail.com>
2021-04-03 09:52:21 +0900

    Update 20210403005134_create_weekly_weather_notification_schedules.rb

--- db/migrate/20210403005134_create_weekly_weather_notification_schedules.rb
+++ db/migrate/20210403005134_create_weekly_weather_notification_schedules.rb
@@ -1,7 +1,8 @@
 class CreateWeeklyWeatherNotificationSchedules < ActiveRecord::Migration[6.0]
   def change
     create_table :weekly_weather_notification_schedules do |t|
-
+      t.references :line_config, null: false
+      t.time :notification_time
       t.timestamps
     end
   end
