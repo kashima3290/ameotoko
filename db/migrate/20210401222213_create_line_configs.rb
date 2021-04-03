249b8b0758c33c033f8627bc86cd99e8c83d3272
kashimaraiki <raiki3290@gmail.com>
2021-04-03 09:51:16 +0900

    Update 20210401222213_create_line_configs.rb

--- db/migrate/20210401222213_create_line_configs.rb
+++ db/migrate/20210401222213_create_line_configs.rb
@@ -1,9 +1,12 @@
 class CreateLineConfigs < ActiveRecord::Migration[6.0]
   def change
     create_table :line_configs do |t|
-      t.references :user, null: false, default: ""
-      t.boolean :notification_flag, null: false, default: true
+      t.references :user, null: false
+      t.boolean :notification_flag, null: false
       t.string :place, null: false
+      t.boolean :weekly_weather_notification_schedule_flag, null: false
+      t.boolean :week_weather_notification_schedule_flag, null: false
+      t.boolean :umbrella_takeaway_flag, null: false
       t.timestamps
     end
   end
