3a659884789d023b634590b9b2e8e8e701ede1a9
kashimaraiki <raiki3290@gmail.com>
2021-04-12 08:41:31 +0900

    Update Weather.js

--- ameotoko_front/src/components/pages/Weather.js
+++ ameotoko_front/src/components/pages/Weather.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
-// import axios from 'axios'
+import axios from 'axios'
 
 class Weather extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/bots')
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
   render() {
     return (
       <h1>Weather</h1>
