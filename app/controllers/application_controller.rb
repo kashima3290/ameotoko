f9aadaec4f083aaea3c66bbff03bc24e92f86974
kashimaraiki <raiki3290@gmail.com>
2021-04-26 09:38:48 +0900

    application_controller miss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByTokenx
+  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_user!
 end
