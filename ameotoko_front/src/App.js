a5a629090fd79ff21623cc1ffc43b3b66d31cea6
kashimaraiki <raiki3290@gmail.com>
2021-04-30 06:45:33 +0900

    Update App.js

--- ameotoko_front/src/App.js
+++ ameotoko_front/src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
 import AppLayout from "./components/templates/AppLayout";
+import SignUp from "./components/pages/SignUp";
+import Login from "./components/pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AppLayout />
+      <Switch>
+        <Route path="/SignUp">
+          <SignUp />
+        </Route>
+        <Route path="/Login">
+          <Login />
+        </Route>
+        {/* 以下のルートに入る前は必ずログイン認証 */}
+        <Route path="/Weather">
+          <AppLayout />
+        </Route>
+        <Route path="/LineConfig">
+          <AppLayout />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
