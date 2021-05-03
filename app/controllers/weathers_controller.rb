042391fcd98d1979203d49a2bf090ff4e30539a0
kashimaraiki <raiki3290@gmail.com>
2021-05-03 14:29:34 +0900

    Update weathers_controller.rb

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,2 +1,9 @@
 class WeathersController < ApplicationController
+  include WeatherJson
+  before_action :now_weather_json
+
+  def index
+    @now_weather = now_weather_json["weather"][0]["description"]
+    render json: @now_weather
+  end
 end
