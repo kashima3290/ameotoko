f19050fd0b06291b250775a7887d19c1659d403b
kashimaraiki <raiki3290@gmail.com>
2021-05-21 09:54:58 +0900

    Update Weather.js

--- ameotoko_front/src/components/pages/Weather.js
+++ ameotoko_front/src/components/pages/Weather.js
@@ -1,42 +1,42 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { errorHandle } from "../functions/errorHandle"
+import { useHistory } from 'react-router-dom'
 
-class Weather extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      posts: []
-    };
-  }
-
-  componentDidMount() {
-    axios.get(process.env.REACT_APP_DEV_RAILS_API_URI + "/weathers")
-    .then(res => {
-      const now_weather = res.data;
-      this.setState({ now_weather });
-    })
-    // エラーハンドリング
-    .catch((error) => {
-      errorHandle(error);
+function Weather(props) {
+  // const hisotry = useHistory()
+  // Rails APIから天気予報を取得
+  axios.get(process.env.REACT_APP_DEV_RAILS_API_URI + "/weathers")
+  .then(response => {
+    const now_weather = response.data;
+    console.log(now_weather);
+  })
+  .catch((error) => { // エラーハンドリング
+    if (error.response) {
+      // エラーが返却された場合
+      // 401 認証エラー
+      if (error.response.status === 401){
+        console.log("エラー出たよ");
+        // window.location = "/Login";
       }
-    )
-  }
-
-  shouldComponentUpdate() {
-    if(this.state.update) {
-      return true;
+      // console.log(error.response.data); // エラー文（devise）
+      // console.log(error.response.headers); // エラー詳細
+      } else if (error.request) {
+        // リクエストは行われましたが、レスポンスは受信しなかった場合
+        // `error.request`は、ブラウザのXMLHttpRequestのインスタンスであり、Node.jsのhttp.ClientRequestのインスタンス
+        console.log(error.request);
+      } else {
+        // 予期しないエラーが発生した場合
+        console.log("Error", error.message);
+      }
     }
-    else {
-      return false;
-    }
-  }
+  )
+  // .finally(function () { // axiosの処理結果によらずいつも実行させたい処理を記述
+  //   }
+  // );
 
-  render() {
-    return (
-      <h1>Weather</h1>
-    );
-  }
-}
+  return (
+    <h1>Weather</h1>
+  );
+};
 
 export default Weather;
